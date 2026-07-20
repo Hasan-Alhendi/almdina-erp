5216bb284c046726bdecad2c78ced7d747f61236
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-20 09:05:30 +0300

    Add Board Usage Analysis report filters

---
+++ almdina_erp/almdina_erp/report/board_usage_analysis/board_usage_analysis.js
@@ -0,0 +1,11 @@
+frappe.query_reports["Board Usage Analysis"] = {
+    filters: [
+        { fieldname: "from_date", label: __("From Date"), fieldtype: "Date" },
+        { fieldname: "to_date", label: __("To Date"), fieldtype: "Date" },
+        { fieldname: "customer", label: __("Customer"), fieldtype: "Link", options: "Customer" },
+        { fieldname: "board_item", label: __("Board Item"), fieldtype: "Link", options: "Item" },
+        { fieldname: "material", label: __("Material"), fieldtype: "Data" },
+        { fieldname: "color", label: __("Color"), fieldtype: "Data" },
+        { fieldname: "thickness_mm", label: __("Thickness MM"), fieldtype: "Float" },
+    ],
+};
