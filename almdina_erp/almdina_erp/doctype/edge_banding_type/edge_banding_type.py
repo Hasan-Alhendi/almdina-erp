5d7d2f4bdca0fee614ed9eb42eecc3f3afd6f046
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-20 05:14:02 +0300

    Sync edge banding stock UOM from mapped Item

--- almdina_erp/almdina_erp/doctype/edge_banding_type/edge_banding_type.py
+++ almdina_erp/almdina_erp/doctype/edge_banding_type/edge_banding_type.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
+
+import frappe
 from frappe.model.document import Document
 
 
 class EdgeBandingType(Document):
-    pass
+    def validate(self) -> None:
+        self.stock_uom = ""
+        if self.item_code:
+            self.stock_uom = frappe.db.get_value("Item", self.item_code, "stock_uom") or ""
