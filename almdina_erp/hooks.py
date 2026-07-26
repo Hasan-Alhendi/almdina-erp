33b16e1567b1ba0cf7225709d213f49be0a030ab
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-26 13:27:47 +0300

    Load special-shape close-button fix

--- almdina_erp/hooks.py
+++ almdina_erp/hooks.py
@@ -41,6 +41,7 @@ doctype_js = {
         "public/js/door_cutting_order_keyboard_columns_ux.js",
         "public/js/door_cutting_order_compact_measurements_ux.js",
         "public/js/door_cutting_order_special_shape_ux.js",
+        "public/js/door_cutting_order_special_shape_close_ux.js",
         "public/js/door_cutting_order_measurement_resilience_ux.js",
         "public/js/door_cutting_order_table_performance_ux.js",
         "public/js/door_cutting_order_cost_invoice_ux.js",
