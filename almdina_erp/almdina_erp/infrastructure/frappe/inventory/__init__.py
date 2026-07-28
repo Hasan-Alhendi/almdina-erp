25b87666c8c7fd3bb8f86a913ba71d2495c1443c
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-28 15:23:36 +0300

    Add Frappe inventory infrastructure package

---
+++ almdina_erp/almdina_erp/infrastructure/frappe/inventory/__init__.py
@@ -0,0 +1,9 @@
+from .stock_availability_repository import (
+    FrappeStockAvailabilityRepository,
+    StockAvailabilityRepositoryError,
+)
+
+__all__ = [
+    "FrappeStockAvailabilityRepository",
+    "StockAvailabilityRepositoryError",
+]
