27b0b56cedfa2987c3380217b3949e5212df5cd5
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-08-01 22:37:28 +0300

    Protect and audit production routing master data

--- almdina_erp/almdina_erp/doctype/production_routing/production_routing.py
+++ almdina_erp/almdina_erp/doctype/production_routing/production_routing.py
@@ -5,6 +5,15 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils import cint
 
+from almdina_erp.almdina_erp.infrastructure.frappe.master_data_audit import (
+    audit_deleted_document,
+    audit_saved_document,
+)
+from almdina_erp.almdina_erp.infrastructure.frappe.master_data_references import (
+    find_link_references,
+    reference_summary,
+)
+
 
 class ProductionRouting(Document):
     def validate(self) -> None:
@@ -13,7 +22,8 @@ class ProductionRouting(Document):
 
         sequences: set[int] = set()
         stage_types: set[str] = set()
-        for row in self.stages:
+        ordered = sorted(self.stages, key=lambda row: cint(row.sequence))
+        for index, row in enumerate(ordered, start=1):
             sequence = cint(row.sequence)
             if sequence <= 0:
                 frappe.throw(_("Routing stage sequence must be greater than zero."))
@@ -23,3 +33,23 @@ class ProductionRouting(Document):
                 frappe.throw(_("Routing stage {0} is duplicated.").format(row.stage_type))
             sequences.add(sequence)
             stage_types.add(row.stage_type)
+            row.idx = index
+
+    def before_trash(self) -> None:
+        references = find_link_references(self.doctype, self.name)
+        if references:
+            frappe.throw(
+                _("This production routing is in use and cannot be deleted: {0}").format(
+                    reference_summary(references)
+                ),
+                frappe.ValidationError,
+            )
+
+    def on_update(self) -> None:
+        audit_saved_document(self)
+
+    def on_trash(self) -> None:
+        audit_deleted_document(self)
+
+
+__all__ = ["ProductionRouting"]
