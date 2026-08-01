a1800b0c29febca75ec351cd6882a343529af806
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-08-01 13:17:10 +0300

    Export permission context use case

--- almdina_erp/almdina_erp/application/security/__init__.py
+++ almdina_erp/almdina_erp/application/security/__init__.py
@@ -1,5 +1,11 @@
 """Security-related application use cases."""
 
+from .permission_context import PERMISSION_CONTEXT_VERSION, build_permission_context
 from .provision_user import PROFILES, provision_user
 
-__all__ = ["PROFILES", "provision_user"]
+__all__ = [
+    "PERMISSION_CONTEXT_VERSION",
+    "PROFILES",
+    "build_permission_context",
+    "provision_user",
+]
