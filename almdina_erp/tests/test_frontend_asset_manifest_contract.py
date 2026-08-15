debc1e5bd0e02a11c49a6b955435566934fdd6ba
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-08-15 23:47:46 +0300

    test: freeze deduplicated drawing primitive loading

--- almdina_erp/tests/test_frontend_asset_manifest_contract.py
+++ almdina_erp/tests/test_frontend_asset_manifest_contract.py
@@ -26,8 +26,6 @@ EXPECTED_APP_INCLUDE_JS = [
 EXPECTED_DOOR_CUTTING_ORDER_JS = [
     "public/js/door_cutting_order/core/door_cutting_order_document_context.js",
     "public/js/permission_context.js",
-    "public/js/door_cutting_order/drawing/door_cutting_order_special_shape_geometry.js",
-    "public/js/door_cutting_order/drawing/door_cutting_order_shape_output_contract.js",
     "public/js/door_cutting_order/printing/door_cutting_order_print_identity.js",
     "public/js/door_cutting_order/cutting_plan/door_cutting_order_cutting_plan_renderer.js",
     "public/js/door_cutting_order/order_entry/door_cutting_order_defaults.js",
