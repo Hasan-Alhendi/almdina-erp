3d187f64e0dee325d2a9686f1747421186e4380c
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-08-17 21:47:40 +0300

    feat: expose reference image workspace API

--- almdina_erp/public/js/door_drawing_v4/professional/workspace_api.js
+++ almdina_erp/public/js/door_drawing_v4/professional/workspace_api.js
@@ -1,10 +1,14 @@
 (() => {
     "use strict";
     const root = window.AlmdinaDoorDrawingProfessional = window.AlmdinaDoorDrawingProfessional || Object.create(null);
-    const SERVICE = "almdina_erp.almdina_erp.services.special_shape_workspace_service";
-    function call(method, args) { return frappe.call({ method: `${SERVICE}.${method}`, args, freeze: false }).then(response => response.message); }
+    const DRAWING_SERVICE = "almdina_erp.almdina_erp.services.special_shape_workspace_service";
+    const REFERENCE_SERVICE = "almdina_erp.almdina_erp.services.special_shape_reference_image_service";
+    function call(service, method, args) { return frappe.call({ method: `${service}.${method}`, args, freeze: false }).then(response => response.message); }
     root.WorkspaceApi = Object.freeze({
-        load(orderName, pieceName) { return call("get_drawing_workspace", { order_name: orderName, piece_name: pieceName }); },
-        save(orderName, pieceName, drawingJson, geometryJson) { return call("save_drawing_workspace", { order_name: orderName, piece_name: pieceName, drawing_json: drawingJson, geometry_json: geometryJson }); },
+        load(orderName, pieceName) { return call(DRAWING_SERVICE, "get_drawing_workspace", { order_name: orderName, piece_name: pieceName }); },
+        save(orderName, pieceName, drawingJson, geometryJson) { return call(DRAWING_SERVICE, "save_drawing_workspace", { order_name: orderName, piece_name: pieceName, drawing_json: drawingJson, geometry_json: geometryJson }); },
+        loadReferenceImage(orderName, pieceName) { return call(REFERENCE_SERVICE, "get_reference_image", { order_name: orderName, piece_name: pieceName }); },
+        saveReferenceImage(orderName, pieceName, imageDataUrl, metadataJson) { return call(REFERENCE_SERVICE, "save_reference_image", { order_name: orderName, piece_name: pieceName, image_data_url: imageDataUrl, metadata_json: metadataJson }); },
+        removeReferenceImage(orderName, pieceName) { return call(REFERENCE_SERVICE, "remove_reference_image", { order_name: orderName, piece_name: pieceName }); },
     });
 })();
