e9ba37f6f526515ce5e5c3ba1809d53d3740f3c5
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-08-28 23:52:00 +0300

    fix(dco): require Plan edit session in lazy readiness

--- almdina_erp/public/js/door_cutting_order/core/door_cutting_order_workspace_asset_registry.js
+++ almdina_erp/public/js/door_cutting_order/core/door_cutting_order_workspace_asset_registry.js
@@ -35,6 +35,7 @@
             readyGlobals: Object.freeze([
                 "AlmdinaPlanWorkspacePresenterAdapter",
                 "AlmdinaCuttingPlanSurfaceBootstrap",
+                "AlmdinaPlanEditSessionUX",
                 "AlmdinaPlanFieldAccessAdapter",
             ]),
         }),
