a9ce907268eafbf956c1e87912983ea232a13f29
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-09-10 15:08:11 +0300

    test: fix Kanban policy test syntax [skip ci]

--- almdina_erp/tests/test_order_status_kanban_policy.py
+++ almdina_erp/tests/test_order_status_kanban_policy.py
@@ -40,4 +40,7 @@ def test_status_kanban_is_read_only_without_frontend_database_writes_or_timers()
     assert 'sortable.option("disabled", true)' in source
     assert 'find(".add-card, .new-card-area").remove()' in source
     assert "frappe.db.set_value" not in source
-    hydration = source.split("function hydrateStatusFilterOptions", 1)[1].split(\n        "function reconcileStatusFilterLayout", 1\n    )[0]\n    assert "setTimeout" not in hydration\n
+    hydration = source.split("function hydrateStatusFilterOptions", 1)[1].split(
+        "function reconcileStatusFilterLayout", 1
+    )[0]
+    assert "setTimeout" not in hydration
